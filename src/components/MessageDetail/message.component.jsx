07842203e490ed9db400a53410cd44bcc87368ab
Tin <hoangtin69@gmail.com>
2022-09-26 04:55:44 +0700

    Show name in chat

--- src/components/MessageDetail/message.component.jsx
+++ src/components/MessageDetail/message.component.jsx
@@ -1,32 +1,40 @@
-import { Avatar, Tooltip } from "antd";
-import React from "react";
-import Swal from "sweetalert2";
-import "./message.style.scss";
-function Message({ text, createAt, isMe, type, src, user, status }) {
+import { Avatar, Tooltip } from 'antd'
+import React from 'react'
+import Swal from 'sweetalert2'
+import './message.style.scss'
+function Message({ candidate, text, createAt, isMe, type, src, user, status }) {
   return (
-    <div className={`message ${isMe ? "me" : ""}`}>
+    <div className={`message ${isMe ? 'me' : ''}`}>
       <div className="wrapper">
         <div>
-          <Tooltip title={user?.name}>
-            <Avatar src={user?.avatarUrl ? user?.avatarUrl : user?.logoUrl} />
+          <Tooltip title={isMe ? user?.name : candidate?.name}>
+            <Avatar
+              src={
+                isMe
+                  ? user?.avatarUrl
+                    ? user?.avatarUrl
+                    : user?.logoUrl
+                  : candidate?.avatarUrl
+              }
+            />
           </Tooltip>
         </div>
         <div className="message__text">
           <Tooltip title={createAt}>
-            {type == "text" ? (
+            {type == 'text' ? (
               <p>{text}</p>
             ) : (
               <audio
                 controls
-                controlsList={status == 1 ? "nodownload" : ""}
-                onTimeUpdate={(e) => {
+                controlsList={status == 1 ? 'nodownload' : ''}
+                onTimeUpdate={e => {
                   if (status == 1) {
-                    const total = e.target.duration;
-                    const limit = total * 0.1;
-                    const currentTime = e.target.currentTime;
+                    const total = e.target.duration
+                    const limit = total * 0.1
+                    const currentTime = e.target.currentTime
                     if (currentTime > limit) {
-                      e.target.pause();
-                      Swal.fire("Kết thúc dự án để nghe toàn bộ!");
+                      e.target.pause()
+                      Swal.fire('Kết thúc dự án để nghe toàn bộ!')
                     }
                   }
                 }}
@@ -38,7 +46,7 @@ function Message({ text, createAt, isMe, type, src, user, status }) {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Message;
+export default Message
